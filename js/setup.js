8b237e4e5466de061dff6b42af360120c0e971e8
Dmitriy Beglov <ditransler@gmail.com>
2017-08-16 22:04:20 +0400

    Added getRandomArrItem function

--- js/setup.js
+++ js/setup.js
@@ -4,3 +4,9 @@ var WIZARDS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', '�
 var WIZARDS_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+function getRandomArrItem(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+
+  return arr[randomIndex];
+}
